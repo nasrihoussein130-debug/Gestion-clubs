7c681bd3a6675aed86ee27a58fb57097fb34a6ea
Nasri <nasrihoussein130@gmail.com>
2026-03-25 20:35:35 +0300

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -651,21 +651,12 @@ tbody tr:hover td { background: rgba(255,255,255,0.02); color: var(--text); }
 
   /* 🔥 IMPORTANT : ON NE CACHE PLUS LE FOOTER */
   .sidebar-footer {
-  display: flex;
-  position: fixed;
-  bottom: 80px;
-  left: 50%;
-  transform: translateX(-50%);
-  width: calc(100% - 20px);
-  justify-content: center;
-  z-index: 300;
-}
-
-.user-pill {
-  width: 100%;
-  max-width: 400px;
-  justify-content: space-between;
-}
+    display: flex;
+    position: fixed;
+    bottom: 70px; /* au-dessus de la navbar */
+    right: 12px;
+    z-index: 300;
+  }
 
   .user-pill {
     padding: 8px 10px;
@@ -791,7 +782,6 @@ tbody tr:hover td { background: rgba(255,255,255,0.02); color: var(--text); }
     gap: 10px;
   }
 }
-  
 `;
 
 
